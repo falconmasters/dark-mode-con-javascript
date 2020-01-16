23923fff5812c47854e7d0cb94f340213e858a09
Carlos Arturo <carlos@falconmasters.com>
2020-01-15 20:31:01 -0600

    Agregando archivos finalizados

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+	btnSwitch.classList.toggle('active');
+});
