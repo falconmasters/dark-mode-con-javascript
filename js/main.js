302d796ee1e850542ed1c2a4ca81c44ea263a82e
Carlos Arturo <carlos@falconmasters.com>
2020-03-03 19:12:29 -0600

    Se agrego localstorage al proyecto

--- js/main.js
+++ js/main.js
@@ -3,4 +3,20 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
 	document.body.classList.toggle('dark');
 	btnSwitch.classList.toggle('active');
+
+	// Guardamos el modo en localstorage.
+	if(document.body.classList.contains('dark')){
+		localStorage.setItem('dark-mode', 'true');
+	} else {
+		localStorage.setItem('dark-mode', 'false');
+	}
 });
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+	document.body.classList.add('dark');
+	btnSwitch.classList.add('active');
+} else {
+	document.body.classList.remove('dark');
+	btnSwitch.classList.remove('active');
+}
